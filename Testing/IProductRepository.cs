84991ce3233f3a81b5d544732a68e979367396f1
Makayla Bryan <makaylabryan@gmail.com>
2024-12-06 14:35:53 -0500

    adding stuff

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -10,5 +10,10 @@ namespace Testing
         IEnumerable<Product> GetAllProducts();
         Product GetProduct(int id);
         void UpdateProduct(Product product);
+
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+
     }
 }
